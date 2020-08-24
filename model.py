fcd1be30d3692b73d06088f09902a6c733a9a539
guedanie <danguerrero93@gmail.com>
2020-08-24 14:48:41 -0500

    new clustering functions

--- model.py
+++ model.py
@@ -1,22 +1,16 @@
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfVectorizer  
 from sklearn.model_selection import train_test_split 
-from sklearn.tree import DecisionTreeClassifier
+
 from pprint import pprint 
 
 import nltk
 import re
 import pandas as pd 
-from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import classification_report
-from sklearn.metrics import confusion_matrix
-from sklearn.metrics import accuracy_score, precision_score, recall_score
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.tree import export_graphviz
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.naive_bayes import MultinomialNB
-# import prepare
+
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.cluster import KMeans
+import prepare
 
 
 
@@ -114,127 +108,116 @@ def preprocessing(df, data_representation, target_variable, ngram_range = (2,2))
 
 
 
+# --------------- # 
+#   Clustering    #
+# --------------- #
 
-# ---------------------- #
-#        Modeling        #
-# ---------------------- #
+def simple_cluster(df, number_of_clusters):
+    df_num = df[["title", "avg_vote", "usa_gross_income", "year", "duration"]]
+    df_num = df_num.set_index("title")
 
-# Decision Tree
+    # First, we need to scale the data
+    minmax = MinMaxScaler()
+    scaled_df = minmax.fit_transform(df_num)
 
-def run_clf(X_train, y_train, max_depth):
-    '''
-    Function used to create and fit decision tree models. It requires a max_depth parameter. Returns model and predictions.
-    '''
-    clf = DecisionTreeClassifier(criterion='entropy', max_depth=max_depth, random_state=123)
-    clf.fit(X_train, y_train)
-    y_pred = clf.predict(X_train)
-    return clf, y_pred
+    # Create an instance of KMeans 
+    kmeans = KMeans(n_clusters=number_of_clusters)
+    # Use fit_predict to cluster the dataset
+    predictions = kmeans.fit_predict(scaled_df)
 
+    df["cluster"] = predictions
+    df["cluster"] = "cluster_" + df.cluster.astype(str)
 
-# KNN
+    return df
 
-def run_knn(X_train, y_train, n_neighbors):
-    '''
-    Function used to create and fit KNN model. Requires to specify n_neighbors. Returns model and predictions.
-    '''
-    knn = KNeighborsClassifier(n_neighbors=n_neighbors, weights='uniform')
-    knn.fit(X_train, y_train)
-    y_pred = knn.predict(X_train)
-    return knn, y_pred
-
-# Random_forest
-
-def run_rf(X_train, y_train, leaf, max_depth):
-    ''' 
-    Function used to create and fit random forest models. Requires to specif leaf and max_depth. Returns model and predictions.
-    '''
-    rf = RandomForestClassifier(random_state= 123, min_samples_leaf = leaf, max_depth = max_depth).fit(X_train, y_train)
-    y_pred = rf.predict(X_train)
-    return rf, y_pred
-
-# Logistic Regression
-
-def run_lg(X_train, y_train):
-    '''
-    Function used to create and fit logistic regression models. Returns model and predictions.
-    '''
-    logit = LogisticRegression().fit(X_train, y_train)
-    y_pred = logit.predict(X_train)
-    return logit, y_pred
-
-# Native Bayes Multinomial
-
-def run_native_bayes(X_train, y_train, alpha=.1):
-    mnb = MultinomialNB(alpha = alpha).fit(X_train, y_train)
-    y_pred = mnb.predict(X_train)
-    return mnb, y_pred
-
-# Evaluation
-
-def create_report(y_train, y_pred):
-    '''
-    Helper function used to create a classification evaluation report, and return it as df
-    '''
-    report = classification_report(y_train, y_pred, output_dict = True)
-    report = pd.DataFrame.from_dict(report)
-    return report
-
-
-def accuracy_report(model, y_pred, y_train):
-    '''
-    Main function used to create printable versions of the classification accuracy score, confusion matrix and classification report.
-    '''
-    report = classification_report(y_train, y_pred, output_dict = True)
-    report = pd.DataFrame.from_dict(report)
-    accuracy_score = f'Accuracy on dataset: {report.accuracy[0]:.2f}'
-
-    labels = sorted(y_train.unique())
-    matrix = pd.DataFrame(confusion_matrix(y_train, y_pred), index = labels, columns = labels)
-
-    return accuracy_score, matrix, report
-
-# ------------------------- #
-#   Creating Prediction     #
-# ------------------------- #
-
-def predict_readme_language(readme):
-    df = acquire_data()
-    df = prepare_data(df)
-
-
-    cv = CountVectorizer(ngram_range = (1,2))
-    X = cv.fit_transform(df.readme_contents.apply(clean).apply(' '.join))
-    y = df["is_top_language"]
-
-    X_train, X_validate, X_test, y_train, y_validate, y_test = split_data(X, y)
-    
-    mnb = MultinomialNB(alpha = 0.7).fit(X_train, y_train)
-       
-    text = prepare_data(readme)
-    
-    X = cv.transform(text.readme_contents.apply(clean).apply(' '.join))
-    
-    prediction = mnb.predict(X)
-    
-    return prediction
-
-# -------------------------- # 
-#   Get Feature Importance   #
-# -------------------------- #
-
-def preprocessing_features(model):
-    
-    df = pd.read_json("data.json")
-    # df = prepare.prep_readme_data(df)
-    df = prepare_data(df)
-
+def complex_cluster(df, number_of_clusters):
+    df["combined_data"] = df.genre + " " + df.director + " " + df.clean_lemmatized
+    df = prepare.prep_readme_data(df, "combined_data")
     tfidf = TfidfVectorizer()
-    X = tfidf.fit_transform(df.readme_contents.apply(clean).apply(' '.join)) 
+    tfidfs = tfidf.fit_transform(df.clean_lemmatized)
+
+    # Create an instance of KMeans to find seven clusters
+    kmeans = KMeans(n_clusters=number_of_clusters)
+    # Use fit_predict to cluster the dataset
+    predictions = kmeans.fit_predict(tfidfs)
+
+    df["cluster_description"] = predictions
+    df["cluster_description"] = "cluster_" + df.cluster_description.astype(str)
+
+    return df
+
+
+
+
+
+
+
     
 
-    pd.Series(dict(zip(tfidf.get_feature_names(), model.feature_importances_))).sort_values().tail(5).plot.barh(title = "Most important words used for modeling", figsize=(10, 8))
+# -------------------- # 
+#   Recommendations    #
+# -------------------- #
 
-    
-    
 
-     
+def simple_movie_recommender(df, test):
+
+    is_in_data = ''
+    for i in df['title']:
+        if test == i :
+            is_in_data = True
+
+    if is_in_data == True:
+        index = df[df.title == test].index
+        
+        genre_type = df[df.title == test].genre.values
+
+        cluster = df[df.title == test].cluster.values
+
+        recommended_movies = df[(df.genre.str.contains(genre_type[0])) & (df.cluster.str.contains(cluster[0]))].sort_values(by="avg_vote", ascending=False)
+        
+        recommended_movies = recommended_movies[recommended_movies.index != index[0]]
+
+        return recommended_movies[["title", "director", "year", "genre", "avg_vote", "usa_gross_income"]].head(25)
+
+    else:
+        return "Movie Not in the Database or Not Spelled Correctly"
+
+
+
+def complex_movie_recommendation(df, test):
+    is_in_data = ''
+    for i in df['title']:
+        if test == i :
+            is_in_data = True
+
+    if is_in_data == True:
+        index = df[df.title == test].index
+
+        genre_type = df[df.title == test].genre.values
+
+        cluster = df[df.title == test].cluster.values
+
+        cluster_desc = df[df.title == test].cluster_description.values
+
+        recommended_movies = (
+            df[(df.genre.str.contains(genre_type[0])) 
+               & ((df.cluster.str.contains(cluster[0]))
+               & (df.cluster_description.str.contains(cluster_desc[0])))]
+            .sort_values(by="avg_vote", ascending=False)
+        )
+
+        recommended_movies = recommended_movies[recommended_movies.index != index[0]]
+        
+        if len(recommended_movies) == 0:
+            recommended_movies = df[(df.genre.str.contains(genre_type[0])) & (df.cluster.str.contains(cluster[0]))].sort_values(by="avg_vote", ascending=False)
+        
+            recommended_movies = recommended_movies[recommended_movies.index != index[0]]
+            
+        if len(recommended_movies) == 0:
+            return "No Recommendations Found"
+
+        recommended_movies[["title", "director", "year", "genre", "avg_vote", "usa_gross_income"]].head(25)
+        return recommended_movies[["title", "director", "year", "genre", "avg_vote", "usa_gross_income"]].head(25)
+
+    else:
+        return "Movie Not in the Database or Not Spelled Correctly"
